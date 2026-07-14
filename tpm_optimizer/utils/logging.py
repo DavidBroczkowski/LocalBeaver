972f0889a3f4c178c5903498d2e7bf084fc775e2
DavidBroczkowski <david.broczkowski@gmail.com>
2026-07-14 16:20:43 +0200

    add logger for use with tpm-optimizer

---
+++ tpm_optimizer/utils/logging.py
@@ -0,0 +1,33 @@
+import time
+from enum import Enum
+from pathlib import Path
+
+class MsgType(Enum):
+    INFO = " [INFO] "
+    DEBUG = " [DEBUG] "
+    ERROR = " [ERROR] "
+    WARN = " [WARN] "
+
+
+class Logger:
+
+    def __init__(self, path: Path):
+        self.path = path
+        path.touch()
+    
+    def log(self, msg: str, type: MsgType = MsgType.INFO, console: bool = True):
+        """
+        Logs a message to the log file at path
+
+        Inputs:
+            - msg: the message to log
+            - console: a boolean value, when True, prints the message to the console
+        """
+        suffix = "[" + time.strftime("%m/%d/%Y %H:%M:%S %z", time.localtime()) + "] "
+        msg = suffix + type.value + msg + "\n"
+        
+        if console:
+            print(msg)
+
+        with open(self.path, 'a', encoding='utf-8') as log_file:
+            log_file.write(msg)
